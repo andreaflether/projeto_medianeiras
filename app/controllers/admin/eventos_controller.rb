c97a886f96cb28ab39fed59ac84e7250f009f161
Andréa <andreaflether@gmail.com>
2019-09-30 16:52:29 -0300

    Creating methods for Events

--- app/controllers/admin/eventos_controller.rb
+++ app/controllers/admin/eventos_controller.rb
@@ -1,4 +1,23 @@
 class Admin::EventosController < ApplicationController
-  def criar 
+
+  def novo
+    @evento = Evento.new
   end
+
+  def criar
+    @evento = Evento.new(evento_params)
+
+    if @evento.save
+      #redirect_to @evento
+    else
+      render 'criar'
+    end
+  end
+
+  private
+
+  def evento_params
+    params.permit(:evento).permit(:nome, :data, :hora, :local, :descricao)
+  end
+
 end

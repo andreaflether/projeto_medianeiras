73f566a80ae5279026b4b3f31c10f740f5313c17
Andréa Alencar <andreaflether@gmail.com>
2019-10-26 19:47:35 -0300

    fixes in a few mobile viewports

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,15 @@ class PagesController < ApplicationController
   def home
   end
 
+  def eventos
+    @eventos = Evento.all
+    @proximos_eventos = Evento.proximos_eventos
+  end
+
+  # def proximos_eventos
+  #   @eventos = Evento.all
+  #   #@eventos = Evento.all.order("created_at ASC")
+  #   @proximos_eventos = Evento.proximos_eventos
+  # end
+
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :proximos_eventos, lambda {
+    where(data: Date.today..Date.today.end_of_month)
+  }
+
 end

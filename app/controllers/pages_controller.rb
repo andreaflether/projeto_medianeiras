0940084b411c33d2c5e113fee6ca2e136290422f
Andréa Alencar <andreaflether@gmail.com>
2019-10-27 21:32:33 -0300

    homepage finished, add login button to home if user is logged in, events showing in pages, changed footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,13 @@
 class PagesController < ApplicationController
-  def home
-  end
 
   def eventos
     @eventos = Evento.all
     @proximos_eventos = Evento.proximos_eventos
   end
 
-  # def proximos_eventos
-  #   @eventos = Evento.all
-  #   #@eventos = Evento.all.order("created_at ASC")
-  #   @proximos_eventos = Evento.proximos_eventos
-  # end
+  def home
+    @eventos = Evento.all
+    @proximos_eventos = Evento.proximos_eventos
+  end
 
 end

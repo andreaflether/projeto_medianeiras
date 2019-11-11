46c5cffe9c9391bcc0c0afb47d17b0acb9ea795c
Andréa <andreaflether@gmail.com>
2019-11-11 11:53:41 -0300

    add validate (atividades/voluntarios) e fix nas views com msg de erro (voluntarios)

--- app/models/atividade.rb
+++ app/models/atividade.rb
@@ -1,4 +1,11 @@
 class Atividade < ApplicationRecord
   has_and_belongs_to_many :pessoas
   has_and_belongs_to_many :voluntarios
+
+  validates :nome, presence: true
+  validates :qtd_max_alunos, presence: true
+  validates :professor, presence: true
+  validates :horario, presence: true
+  validates :dias_semana, presence: true
+
 end
--- app/models/voluntario.rb
+++ app/models/voluntario.rb
@@ -1,6 +1,10 @@
 class Voluntario < ApplicationRecord
   has_and_belongs_to_many :atividades
 
+  validates :nome, presence: true
+  validates :dt_nascimento, presence: true
+  validates :escolaridade, presence: true
+
   def form_presente
     presenca ? "Sim" : "Não"
   end

9e429f78eb72b277f1ba943332ce00aab6c4ee4c
Andréa Alencar <andreaflether@gmail.com>
2019-11-29 10:22:22 -0300

    input de imagem nas views, fix figaro_envs

--- db/migrate/20191129121329_delete_link_imagem_from_atividades.rb
+++ db/migrate/20191129121329_delete_link_imagem_from_atividades.rb
@@ -1,4 +1,5 @@
 class DeleteLinkImagemFromAtividades < ActiveRecord::Migration[5.2]
   def change
+    remove_column :atividades, :link_imagem, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_194720) do
+ActiveRecord::Schema.define(version: 2019_11_29_121329) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "atividades", force: :cascade do |t|
     t.string "nome"
@@ -22,7 +43,6 @@ ActiveRecord::Schema.define(version: 2019_11_24_194720) do
     t.integer "qtd_max_alunos"
     t.integer "tipo"
     t.text "descricao"
-    t.string "link_imagem"
   end
 
   create_table "atividades_pessoas", id: false, force: :cascade do |t|

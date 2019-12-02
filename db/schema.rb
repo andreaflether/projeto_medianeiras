586c511cbdd3b03624fe59834ef6ef24a1133544
Andréa Alencar <andreaflether@gmail.com>
2019-12-02 11:21:28 -0300

    remoção de campos redundantes nas tabelas, fix no display de usuários/pessoas/voluntários

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2019_11_29_121329) do
+ActiveRecord::Schema.define(version: 2019_12_01_234056) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -35,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_121329) do
 
   create_table "atividades", force: :cascade do |t|
     t.string "nome"
-    t.string "professor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "horario"
@@ -65,16 +52,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_121329) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "mensagems", force: :cascade do |t|
-    t.string "nome"
-    t.string "email"
-    t.integer "assunto"
-    t.string "numero"
-    t.text "mensagem"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "pessoas", force: :cascade do |t|
     t.string "nome"
     t.integer "idade"
@@ -88,7 +65,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_121329) do
     t.boolean "estuda"
     t.string "escola"
     t.string "serie"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -103,7 +79,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_121329) do
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
     t.string "name"
-    t.boolean "situacao"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

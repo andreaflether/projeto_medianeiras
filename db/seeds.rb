728b6f49b7247efeeebd969fcd6434bb80aef27e
Andréa Alencar <andreaflether@gmail.com>
2019-12-11 11:20:34 -0300

    fix na view de voluntário mostrando data de nascimento ao invés da data de entrada

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
     nome: Faker::Name.name,
     dt_nascimento: Faker::Date.birthday(min_age: 18, max_age: 65),
     escolaridade: 'Ensino Superior',
-    especializacao: Faker::Educator.subject
+    especializacao: Faker::Educator.subject,
+    dt_entrada: Faker::Date.in_date_period
   }])
 
 end
